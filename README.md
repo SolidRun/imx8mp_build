ac5ed8b066a36e8c8b31f3fa425ba7059e3ee04e
Yazan Shhady <yazan.shhady@solid-run.com>
2022-09-22 22:01:54 +0300

    Add support to building the project with Docker

--- README.md
+++ README.md
@@ -5,6 +5,25 @@ Main intention of this repository is to a buildroot based build environment for 
 
 The build script provides ready to use images that can be deployed on micro SD and future on eMMC.
 
+## Build with Docker
+A docker image providing a consistent build environment can be used as below:
+
+1. build container image (first time only)
+   ```
+   sudo docker build -t imx8mp_build docker
+   # optional with an apt proxy, e.g. apt-cacher-ng
+   # docker build --build-arg APTPROXY=http://127.0.0.1:3142 -t imx8mp_build docker
+   ```
+2. invoke build script in working directory
+   ```
+   sudo docker run -i -t -v "$PWD":/work imx8mp_build -u $(id -u) -g $(id -g)
+   ```
+
+### rootless Podman
+
+Due to the way podman performs user-id mapping, the root user inside the container (uid=0, gid=0) will be mapped to the user running podman (e.g. 1000:100).
+Therefore in order for the build directory to be owned by current user, `-u 0 -g 0` have to be passed to *docker run*.
+
 ## Build with host tools
 Simply running ./runme.sh, it will check for required tools, clone and build images and place results in images/ directory.
 
---
+++ docker/entry.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -e
+# Copyright 2018-2019 Josua Mayer <josua@solid-run.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+# include shFlags library
+. /shflags
+
+# declare flags
+DEFINE_integer 'uid' 1000 'User ID to run as' 'u'
+DEFINE_integer 'gid' 100 'Group ID to run as' 'g'
+
+# parse flags
+FLAGS "$@" || exit 1
+eval set -- "${FLAGS_ARGV}"
+
+# create build-user and group if not root
+user=root
+if [ "x${FLAGS_uid}" != "x0" ]; then
+	groupadd -g ${FLAGS_gid} build 2>/dev/null || true
+	useradd -s /bin/bash -u ${FLAGS_uid} -g ${FLAGS_gid} -m -G sudo build
+	echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+	user=build
+fi
+
+# preconfigure git identity
+sudo -u $user git config --global user.name "IMX8M Toolchain Container"
+sudo -u $user git config --global user.email "support@solid-run.com"
+
+cd /work
+# now run the build script as the build user
+sudo -u $user -E ./runme.sh
