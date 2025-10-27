cce4a931be02c13e2e6a2567ba6a3aab7f78a364
Josua Mayer <josua@solid-run.com>
2025-10-27 18:22:45 +0100

    uboot: sync with imx8mm/imx8mp

--- runme.sh
+++ runme.sh
@@ -7,7 +7,7 @@ declare -A GIT_REL GIT_COMMIT GIT_URL
 GIT_REL[imx-atf]=lf-6.6.36-2.1.0
 GIT_URL[imx-atf]=https://github.com/nxp-imx/imx-atf.git
 GIT_REL[uboot-imx]=lf-6.6.52-2.2.0-sr-imx8
-GIT_COMMIT[uboot-imx]=0da4b811f8096dc81a4de5502e51815dd0b683e0
+GIT_COMMIT[uboot-imx]=a820407959a9e6c086704e3e1ebc26ee7745927b
 GIT_URL[uboot-imx]=https://github.com/SolidRun/u-boot.git
 GIT_REL[linux-imx]=lf-6.6-sr-imx8
 GIT_COMMIT[linux-imx]=009834fcd03cf28e9e0282197776e0e35dff751a
