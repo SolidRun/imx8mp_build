535b1fdd2b06be20cd0c04ed010d65b4c9c8fabd
Josua Mayer <josua@solid-run.com>
2025-10-31 12:06:41 +0100

    uboot: fix hummingboard pulse fdtfile fall-back for empty tlv

--- runme.sh
+++ runme.sh
@@ -7,7 +7,7 @@ declare -A GIT_REL GIT_COMMIT GIT_URL
 GIT_REL[imx-atf]=lf-6.6.36-2.1.0
 GIT_URL[imx-atf]=https://github.com/nxp-imx/imx-atf.git
 GIT_REL[uboot-imx]=lf-6.6.52-2.2.0-sr-imx8
-GIT_COMMIT[uboot-imx]=28edafff0b0c8f4493590079746650a2b8dab271
+GIT_COMMIT[uboot-imx]=7ee00406e63caa4d9176e8a337621fd9b82f68a5
 GIT_URL[uboot-imx]=https://github.com/SolidRun/u-boot.git
 GIT_REL[linux-imx]=lf-6.6-sr-imx8
 GIT_COMMIT[linux-imx]=009834fcd03cf28e9e0282197776e0e35dff751a
