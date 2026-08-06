3064a022f12aa9c463476dcc4d0355d51748bcaa
Josua Mayer <josua@solid-run.com>
2026-08-06 09:39:32 +0300

    uboot: fix reserved-memory conflicts

--- runme.sh
+++ runme.sh
@@ -7,7 +7,7 @@ declare -A GIT_REL GIT_COMMIT GIT_URL
 GIT_REL[imx-atf]=lf-6.6.36-2.1.0
 GIT_URL[imx-atf]=https://github.com/nxp-imx/imx-atf.git
 GIT_REL[uboot-imx]=lf-6.6.52-2.2.0-sr-imx8
-GIT_COMMIT[uboot-imx]=28edafff0b0c8f4493590079746650a2b8dab271
+GIT_COMMIT[uboot-imx]=3a25cde344b84f8dd6cf25af1019c18cb51ba650
 GIT_URL[uboot-imx]=https://github.com/SolidRun/u-boot.git
 GIT_REL[linux-imx]=lf-6.6-sr-imx8
 GIT_COMMIT[linux-imx]=04d5b42d65c806ce96a2ee8f4a3c586906c75a9c
